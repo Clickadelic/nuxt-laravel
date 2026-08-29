123b3305cb2c03517d43a4b572ad4296412ddbbf
Clickadelic <toby.hopp@gmail.com>
2026-08-29 03:15:36 +0200

    Sanctum added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', function () {
@@ -14,4 +15,8 @@ Route::get('/hello-world', function () {
 		'status' => 'ok',
 		'service' => 'hello world is here',
 	]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+	return $request->user();
 });
