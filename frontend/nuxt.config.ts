05679fcbcb3e12bcec3dd63c73e8503e64959e69
Clickadelic <toby.hopp@gmail.com>
2026-08-29 02:10:55 +0200

    Quote err config fix

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	runtimeConfig: {
 		public: {
-			apiBase: process.env.NUXT_PUBLIC_API_BASE"
+			apiBase: process.env.NUXT_PUBLIC_API_BASE
 		}
 	}
 });
